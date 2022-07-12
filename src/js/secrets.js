eef73f49ad8ba396809b6ea1085a9c6f23874d02
Audiemar <45100426+Audiemar@users.noreply.github.com>
2022-07-12 10:24:03 -0700

    Add files via upload

---
+++ src/js/secrets.js
@@ -0,0 +1,8 @@
+export const PAYMENT_ADDR = 'addr1qxx3nzhwqczartkuh7xymf7r2xrys4pt5x2mn0ryj79357y7y6ll6a8g5pp4xq6hjzngdcwgfct46ephuca2qw3z4yvq0sqxcg';
+    export const BLOCKFROST_PROJ = 'WalletNotes';
+    export const MINT_PRICE = '6000000';
+    export const MINT_REBATE = '1';
+    export const LOWER_LIMIT = '1';
+    export const UPPER_LIMIT = '1';
+	export const REQUIRED_POLICY_KEY = '';
+    export const REQUIRED_POLICY_MIN = 0;
