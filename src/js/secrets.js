867c1f04daaa59f9b0b4cc02a1928a0a658e3ae6
Audiemar <45100426+Audiemar@users.noreply.github.com>
2022-07-12 10:51:09 -0700

    Update secrets.js

--- src/js/secrets.js
+++ src/js/secrets.js
@@ -1,7 +1,7 @@
 export const PAYMENT_ADDR = 'addr1qxx3nzhwqczartkuh7xymf7r2xrys4pt5x2mn0ryj79357y7y6ll6a8g5pp4xq6hjzngdcwgfct46ephuca2qw3z4yvq0sqxcg';
     export const BLOCKFROST_PROJ = 'WalletNotes';
     export const MINT_PRICE = '1000000';
-    export const MINT_REBATE = '1';
+    export const MINT_REBATE = '0';
     export const LOWER_LIMIT = '1';
     export const UPPER_LIMIT = '1';
 	export const REQUIRED_POLICY_KEY = '';
