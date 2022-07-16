a61450252e9da6b15f98bee9bc7b85a713fee4c8
Audiemar <45100426+Audiemar@users.noreply.github.com>
2022-07-16 06:12:30 -0700

    Add files via upload

---
+++ src/js/tools/drop-payment.js
@@ -0,0 +1,66 @@
+import * as Selector from "./wallet-selector.js";
+import * as LucidInst from "./lucid-inst.js";
+
+const MINT_COUNT_DOM = "#mint-count";
+
+function updateMintCount(count, lowerLimit, upperLimit) {
+  var boundedCount = Math.max(lowerLimit, Math.min(upperLimit, count));
+  document.querySelector(MINT_COUNT_DOM).value = boundedCount;
+}
+
+function getCurrentCount() {
+  return parseInt(document.querySelector(MINT_COUNT_DOM).value);
+}
+
+export function decreaseMintCount(e, lowerLimit, upperLimit) {
+  e.preventDefault();
+  updateMintCount(getCurrentCount() - 1, lowerLimit, upperLimit);
+}
+
+export function increaseMintCount(e, lowerLimit, upperLimit) {
+  e.preventDefault();
+  updateMintCount(getCurrentCount() + 1, lowerLimit, upperLimit);
+}
+
+export function validateMintCount(e, lowerLimit, upperLimit) {
+  updateMintCount(getCurrentCount(), lowerLimit, upperLimit);
+}
+
+function toastPaymentError(message) {
+  Toastify({ text: message, duration: 3000 }).showToast();
+}
+
+export function mintNow(e, blockfrostKey, paymentAddr, price) {
+  e.preventDefault();
+  if (!Selector.isWalletConnected()) {
+    toastPaymentError('Please connect a wallet before minting using "Connect Wallet" button (desktop only)');
+    return;
+  }
+
+  Selector.enableWallet(Selector.getConnectedWallet()).then(wallet => {
+    var lucidInst = LucidInst.getLucidInstance(blockfrostKey);
+    if (!lucidInst) {
+      toastPaymentError('Unable to initialize Lucid, check your secrets.js file for a network mismatch');
+      return;
+    }
+
+    lucidInst.then(lucid => {
+      lucid.selectWallet(wallet);
+      var paymentAmount = getCurrentCount() * price;
+      const tx = lucid.newTx()
+                      .payToAddress(paymentAddr, { lovelace: paymentAmount })
+                      .complete()
+                      .then(tx =>
+                          tx.sign().complete().then(signedTx =>
+                            signedTx.submit().then(txHash =>
+                              Toastify({
+                                text: `Successfully sent money for minting in tx: ${txHash}`,
+                                duration: 6000
+                              }).showToast()
+                            )
+                          )
+                      )
+                      .catch(err => toastPaymentError(`Transaction error occurred: ${JSON.stringify(err)}`));
+    }).catch(_ => toastPaymentError('Wallet initialization failed (are you on the right network?)'));
+  }).catch(err => toastPaymentError(`Unknown error occurred, contact developer: ${err}`));
+}
